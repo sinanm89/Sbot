cd17d7283819946df3897558baa95f5ae8978a4b
sinanm89 <sinanm89@gmail.com>
2015-08-08 03:17:18 +0300

    whatsapp bot init

---
+++ run.py
@@ -0,0 +1,55 @@
+from layer_old import EchoLayer
+from yowsup.layers.auth                        import YowAuthenticationProtocolLayer
+from yowsup.layers.protocol_messages           import YowMessagesProtocolLayer
+from yowsup.layers.protocol_receipts           import YowReceiptProtocolLayer
+from yowsup.layers.protocol_acks               import YowAckProtocolLayer
+from yowsup.layers.network                     import YowNetworkLayer
+from yowsup.layers.coder                       import YowCoderLayer
+from yowsup.layers.auth                        import YowCryptLayer, YowAuthenticationProtocolLayer, AuthError
+from yowsup.layers.coder                       import YowCoderLayer
+from yowsup.layers.network                     import YowNetworkLayer
+from yowsup.layers.protocol_messages           import YowMessagesProtocolLayer
+from yowsup.layers.protocol_media              import YowMediaProtocolLayer
+from yowsup.layers.stanzaregulator             import YowStanzaRegulator
+from yowsup.layers.protocol_receipts           import YowReceiptProtocolLayer
+from yowsup.layers.protocol_acks               import YowAckProtocolLayer
+from yowsup.layers.logger                      import YowLoggerLayer
+from yowsup.layers.protocol_iq                 import YowIqProtocolLayer
+from yowsup.layers.protocol_calls              import YowCallsProtocolLayer
+from yowsup.stacks import YowStack
+from yowsup.common import YowConstants
+from yowsup.layers import YowLayerEvent
+from yowsup.stacks import YowStack, YOWSUP_CORE_LAYERS
+from yowsup import env
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+CREDENTIALS = ("905396815006", "N35sXununbpdtxIKQATBMv8gCrM=") # replace with your phone and password
+
+if __name__==  "__main__":
+    from yowsup.layers.axolotl                     import YowAxolotlLayer
+    # layers = (
+    #     EchoLayer,
+    #     (YowAuthenticationProtocolLayer, YowMessagesProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer, YowAxolotlLayer)
+    # ) + YOWSUP_CORE_LAYERS
+
+    layers = (
+    EchoLayer,
+    (YowAuthenticationProtocolLayer, YowMessagesProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer),
+    YowAxolotlLayer
+    )  + YOWSUP_CORE_LAYERS
+    
+    stack = YowStack(layers)
+    stack.setProp(YowAuthenticationProtocolLayer.PROP_CREDENTIALS, CREDENTIALS)         #setting credentials
+    stack.setProp(YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0])    #whatsapp server address
+    stack.setProp(YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN)              
+    stack.setProp(YowCoderLayer.PROP_RESOURCE, env.CURRENT_ENV.getResource())          #info about us as WhatsApp client
+
+    stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))   #sending the connect signal
+    
+    # try:
+    stack.loop()
+    # except Exception, e:
+        # print '---'
+        # print e
